852a1f24e590819522246f571c79ac3ea75ce39c
Scott Dodelson <dodelson@fnal.gov>
2018-11-16 10:43:58 -0500

    edits in response to Wayne and new figure

--- source/kernel.py
+++ source/kernel.py
@@ -2,21 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 import distance as d
 
-z=np.arange(0.1,10.,.1)
+z=np.arange(0.01,10.,.01)
 n=np.size(z)
 chistar=d.chi(1189.)
 chia=np.zeros(n)
 k1=np.zeros(n)
 for i,za in enumerate(z):
 	chia[i]=d.chi(za)
-	k1[i]=d.hubinv(za)*(chistar-chia[i])/(chistar**2)
-plt.plot(z,(1+z)*d.hubinv(z)/chistar,'b',label='Time Delay')
-plt.plot(z,(1+z)*k1,'--r',label='Deflection Angle')
+	k1[i]=d.hubinv(za)*(chistar-chia[i])/(chistar*chia[i])
+plt.plot(z,d.hubinv(z)/chistar,'b',label='Time Delay')
+plt.plot(z,k1,'--r',label='Deflection Angle')
 #plt.xscale('log')
-plt.axis([0.1,10,0,.4])
+plt.axis([0,10,0.003,100])
 plt.xlabel('$z$',fontsize=18)
-plt.xscale('log')
+plt.yscale('log')
 plt.legend()
-plt.ylabel('$K(z_*,z)$',fontsize=18)
+plt.ylabel('$K(z)$',fontsize=18)
 plt.savefig('../plots/kernel.pdf')
 
